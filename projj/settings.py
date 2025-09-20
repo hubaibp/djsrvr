b0ded5c7575fb0662ad6258b65bc7bf43b472b34
hubaibp <27hubaib@gmail.com>
2025-09-20 13:19:46 +0530

    hello

--- projj/settings.py
+++ projj/settings.py
@@ -126,6 +126,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # settings.py
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",          # React dev server
